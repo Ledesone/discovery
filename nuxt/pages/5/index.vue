fca8ed95901583649fcde708cbd11b760369b714
hideyasu <kutuzov1228@gmail.com>
2019-04-07 14:55:16 +0900

    bootstrapの導入

--- nuxt/pages/5/index.vue
+++ nuxt/pages/5/index.vue
@@ -1,22 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <!-- Default -->
-      <button type="button" class="btn btn-default">Default</button>
-      <!-- Primary -->
-      <button type="button" class="btn btn-primary">Primary</button>
-      <!-- Success -->
-      <button type="button" class="btn btn-success">Success</button>
-      <!-- Info -->
-      <button type="button" class="btn btn-info">Info</button>
-      <!-- Warning -->
-      <button type="button" class="btn btn-warning">Warning</button>
-      <!-- Danger -->
-      <button type="button" class="btn btn-danger">Danger</button>
-      <!-- Link -->
-      <button type="button" class="btn btn-link">Link</button>
-    </div>
-  </section>
+  <div class="row">
+    <div class="col-sm-2" style="background-color:red;">Red</div>
+    <div class="col-sm-8" style="background-color:blue;">Blue</div>
+    <div class="col-sm-2" style="background-color:yellow;">Yellow</div>
+  </div>
 </template>
 
 <script>
