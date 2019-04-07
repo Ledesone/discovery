deb81c9fbb26dab526db1db38527f7b2a827f259
yanase-y <snowsound_yuki0909@icloud.com>
2019-04-07 16:51:55 +0900

    P6

---
+++ nuxt/pages/6/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <div>
+        <img src="" alt="" width="" height="">
+        <div class="face"><i class="face-icon" area-hidden="true"></i></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+    .face{
+    width: 50px;
+    height: 50px;
+    color: ; 
+    background-color: ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: ;
+    border-radius: ;
+    cursor: pointer;
+    }
+
+    
+</style>
