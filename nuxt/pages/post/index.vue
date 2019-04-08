49f32bd8cf179a25be1ffe8caaa915c0e2f6bce2
hideyasu <kutuzov1228@gmail.com>
2019-04-08 13:24:48 +0900

    途中

--- nuxt/pages/post/index.vue
+++ nuxt/pages/post/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <section class="container">
-    <div>
-      post
+  <div class="row">
+    <div class="container-fluid">
+      <div>
+        <div class="text-center">
+          <img src="https://www.motex.co.jp/nomore/wp-content/uploads/2015/10/a11b4bb3ba448d1fa402ac3dc62cc91f.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+      </div>
     </div>
-  </section>
+    <div class="container-fluid">
+      <div>
+        <div class="text-center">
+          <img src="https://www.motex.co.jp/nomore/wp-content/uploads/2015/10/a11b4bb3ba448d1fa402ac3dc62cc91f.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
